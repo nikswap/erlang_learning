a8821953b302b7007b389afe246916a00888a935
nikswap <nikswap@github.com>
2023-01-31 21:03:47 +0100

    Added more code

---
+++ program_template.erl
@@ -0,0 +1,22 @@
+-module(MODULENAME).
+-compile(export_all). 
+
+start() ->
+    spawn(fun() ->
+		  loop([]) end).
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+	{Pid, Response} ->
+	    Response
+    end.
+
+loop(X) ->
+    receive
+	Any ->
+	    io:format("Received:~p~n", [Any]),
+	    loop(X)
+    end.
+
+	    
